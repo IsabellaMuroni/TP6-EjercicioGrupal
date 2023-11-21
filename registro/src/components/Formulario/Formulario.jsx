b16d798765c852f19151d403791eb1b3c835f96e
IsabellaMuroni <isa_sk_@hotmail.com>
2023-11-21 18:51:25 -0300

    Se agregó validación

--- registro/src/components/Formulario/Formulario.jsx
+++ registro/src/components/Formulario/Formulario.jsx
@@ -19,31 +19,31 @@ const Formulario = () => {
                 <form onSubmit={handleSubmit} className="row g-3 bg-light-subtle rounded border border-secondary" >
                     <div className="col-md-6">
                         <label htmlFor="nombre" className="form-label" >Nombre</label>
-                        <input type="text" value = {mensaje} onChange={handleChange} className="form-control"  />
+                        <input type="text" value = {mensaje} onChange={handleChange} className="form-control" required />
                     </div>
                     <div className="col-md-6">
                         <label htmlFor="apellido" className="form-label">Apellido</label>
-                        <input type="text" className="form-control"  />
+                        <input type="text" className="form-control" required />
                     </div>
                     <div className="col-md-6">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input type="email" className="form-control"  />
+                        <input type="email" className="form-control" required />
                     </div>
                     <div className="col-md-6">
                         <label htmlFor="teleefono" className="form-label">Teléfono</label>
-                        <input type="number" className="form-control" id="telefono"/>
+                        <input type="number" className="form-control" id="telefono" required/>
                     </div>
                     <div className="col-12">
                         <label htmlFor="pass" className="form-label">
                         Password
                         </label>
-                        <input type="password" className="form-control" id="pass" />
+                        <input type="password" className="form-control" id="pass" required/>
                     </div>
                     <div className="col-12">
                         <label htmlFor="passw" className="form-label">
                         Confirmar Password
                         </label>
-                        <input type="password" className="form-control" id="passw" />
+                        <input type="password" className="form-control" id="passw" required />
                     </div>
                     
                     <div className="col-12">
